555ec0c11b7ecb19d5b92a2b2996c39357453e55
iaranda <ivanarandavillanueva@cdpjosecabrera.es>
2022-05-19 13:58:40 +0200

    Prueba github

--- TablaMultiplicar/src/Main.java
+++ TablaMultiplicar/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("prueba git");
+		System.out.println("Prueba github");
 		// TODO Auto-generated method stub
 		System.out.println("Tabla de multiplicar");
 		Scanner leer=new Scanner (System.in);
